d401322788556c3aefdebab56b6b32d58281c3a8
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-09-30 12:28:56 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,3 +6,6 @@ function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = songTitle;
 }
 
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName]
+}
